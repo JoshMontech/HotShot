ad53003e53cd86368147a8486c3ca6ed7d41c1a2
iOS4ever <jerry.pon@utexas.edu>
2016-10-09 22:17:00 -0500

    Add CameraManager

--- HotShot/ViewController.swift
+++ HotShot/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CameraManager
 
 class ViewController: UIViewController {
 
+    let cameraManager = CameraManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        cameraManager.addPreviewLayerToView(self.view)
     }
 
     override func didReceiveMemoryWarning() {
