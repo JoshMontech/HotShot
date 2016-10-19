2c66deb4f1ee447af1baf4e0ff17f8412bd4a0d7
jakemayo254 <jakemayo254@gmail.com>
2016-10-18 20:05:11 -0500

    jakes additions

---
+++ HotShot/LogInViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LogInViewController.swift
+//  HotShot
+//
+//  Created by Jake Mayo on 10/18/16.
+//  Copyright © 2016 Jerry. All rights reserved.
+//
+
+import Foundation
---
+++ HotShot/NewUserViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NewUserViewController.swift
+//  HotShot
+//
+//  Created by Jake Mayo on 10/18/16.
+//  Copyright © 2016 Jerry. All rights reserved.
+//
+
+import Foundation
