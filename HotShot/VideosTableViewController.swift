80714b9045adda6ef1db20299c49da794138b9b9
Joshua Montgomery <JoshMontgomery@utexas.edu>
2016-10-20 19:57:38 -0500

    added default tableview stuff and standardized icons

--- HotShot/VideosTableViewController.swift
+++ HotShot/VideosTableViewController.swift
@@ -10,7 +10,31 @@ import UIKit
 
 class VideosTableViewController: UITableViewController {
     
+    var videoCount:Int = 0
+    
     override func viewDidLoad() {
         self.navigationController?.navigationBar.isHidden = false
+        videoCount = getVideoCount()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videoCount
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "videoPreviewCell", for: indexPath) as! VideoTableCell
+        cell.videoNameLabel.text = "derp"
+        cell.videoPreviewThumbnail.image = nil
+        return cell
+    }
+    
+    //helper functions
+    func getVideoCount() -> Int {
+        //once videos are working properly, will have more sophisticated handling
+        return 4
     }
 }
