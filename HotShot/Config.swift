2b15502df8e717f5bd593eb655404ef5737e11a3
iOS4ever <jerry.pon@utexas.edu>
2016-11-15 13:57:19 -0600

    Add custom yellow color

--- HotShot/Config.swift
+++ HotShot/Config.swift
@@ -12,5 +12,6 @@ class Config {
     static let sharedInstance = Config()
     let recordGreen = UIColor(red:0.31, green:0.56, blue:0.13, alpha:1.00)
     let burntOrange = UIColor(red: 191/255, green: 87/255, blue: 0, alpha: 1)
+    let starYellow = UIColor(red:0.91, green:0.64, blue:0.15, alpha:1.00)
 
 }
